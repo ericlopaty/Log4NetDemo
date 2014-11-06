def721c1c67ad66beeb0d6b7dcae0d1441800209
Eric Lopaty <ericlopaty@gmail.com>
2014-11-06 16:02:48 -0500

    First commit.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        public static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Log.Info("Application Starting");
+            try
+            {
+                Log.Debug("Start of Try block");
+                // do some work
+                Log.Debug("End of Try block");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error caught", ex);
+            }
+            Pause("Done");
+            Log.Info("Application Ending");
+        }
+
+        public static void Pause(string message)
+        {
+            Console.Write(message);
+            Console.ReadLine();
+        }
+    }
+}
